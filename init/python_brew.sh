6e65ef530f32e1172cb63304a37d15acf2bbd0b8
Laurent Perrinet <laurent.perrinet@gmail.com>
2013-01-29 14:08:16 +0100

    fixing installation of the python toolchain

--- init/python_brew.sh
+++ init/python_brew.sh
@@ -12,10 +12,16 @@ brew upgrade
 brew install python
 
 pip install --upgrade distribute
+# editing environment
+pip install -U pyzmq
 pip install -U ipython
 brew install pyqt
 pip install -U sphinx
 pip install -U spyder
+
+# testing
+pip install -U nose
+easy_install pyreport
 
 # numpy et al
 brew install gfortran
@@ -25,21 +31,21 @@ pip install -U scipy
 pip install -U matplotlib
 # pip install git+git://github.com/matplotlib/matplotlib.git
 
-#pip install pyOpenGL
-#pip install glumpy
 
 # pyglet
-pip install -U pyglet
+pip install -U Opengl
+pip install -U glumpy
+#easy_install pyglet
 
-#pip install Mercurial
+pip install Mercurial
 #pip install https://pyglet.googlecode.com/hg/
 #easy_install pyobjc-core
 #easy_install pyobjc
-#hg clone https://pyglet.googlecode.com/hg/ pyglet
-#cd libs/pyglet
-#sudo python setup.py install
-#cd ../..
-#rm -fr pyglet
+hg clone https://pyglet.googlecode.com/hg/ pyglet
+cd pyglet
+python setup.py install --user
+cd ..
+rm -fr pyglet
 
 #hg clone https://code.google.com/r/evilphillip-cocoa-ctypes2/
 #cd evilphillip-cocoa-ctypes2
