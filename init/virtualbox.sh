0679aa5cd146d7c86fd4601328e5d433164730c5
Laurent Perrinet <laurent.perrinet@gmail.com>
2013-03-22 14:20:49 +0100

    new formula to install VirtualBox on MacOsX

--- init/virtualbox.sh
+++ init/virtualbox.sh
@@ -1,2 +1,5 @@
-
+cd /tmp
+wget http://download.virtualbox.org/virtualbox/4.2.10/VirtualBox-4.2.10-84104-OSX.dmg
+hdiutil attach VirtualBox-4.2.10-84104-OSX.dmg
 sudo installer -pkg /Volumes/VirtualBox/VirtualBox.pkg -target /
+hdiutil detach /Volumes/VirtualBox
