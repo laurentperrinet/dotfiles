98d7ebec4f7bca12f7f235e64798e899d7bd8c8c
Laurent Perrinet <laurent.perrinet@gmail.com>
2013-05-14 11:29:45 +0200

    install scripts for texlive : better pattern for key generation

--- init/osx_install_tex_live.sh
+++ init/osx_install_tex_live.sh
@@ -3,7 +3,7 @@ wget http://ctan.ijs.si/mirror/tlpretest/mactex-2013.pkg # pre-release of TexLiv
 sudo installer -pkg mactex-2013.pkg -target /
 
 # setting some preferences in bibdesk (see http://invibe.net/LaurentPerrinet/SciBlog/2013-03-06):
-defaults write edu.ucsd.cs.mmccrack.bibdesk "Cite Key Format" -string "%a1%y%u0"
+defaults write edu.ucsd.cs.mmccrack.bibdesk "Cite Key Format" -string "%p1%y%u0"
 defaults write edu.ucsd.cs.mmccrack.bibdesk BDSKLocalFileFormatKey -string "%f{Cite Key}%n0%e"
 defaults write edu.ucsd.cs.mmccrack.bibdesk "Path to the papers folder" -string "Biblio"
 defaults write edu.ucsd.cs.mmccrack.bibdesk "Cite String" -string "citep"
