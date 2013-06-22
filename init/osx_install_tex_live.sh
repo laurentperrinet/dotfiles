af0acf23c35280a85f2513e4ca00786de6af653e
meduz <orez.org@gmail.com>
2013-06-22 11:37:38 +0200

    installing texlive with a downloaded package

--- init/osx_install_tex_live.sh
+++ init/osx_install_tex_live.sh
@@ -1,6 +1,7 @@
 # fetch and install texlive
 
-wget http://ctan.ijs.si/mirror/tlpretest/mactex-2013.pkg # pre-release of TexLive 2013
+#wget http://ctan.ijs.si/mirror/tlpretest/mactex-2013.pkg # pre-release of TexLive 2013
+cd ~/pool/soft
 sudo installer -pkg mactex-2013.pkg -target /
 
 # point to the right URL, see http://invibe.net/LaurentPerrinet/SciBlog/2013-06-12
