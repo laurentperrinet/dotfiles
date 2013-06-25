fb745d5278315f0af609f92a355cfddfe589f803
Laurent Perrinet <laurent.perrinet@gmail.com>
2013-06-25 16:47:29 +0200

    including mayavi in the scientific python toolchain

--- init/osx_brew_science.sh
+++ init/osx_brew_science.sh
@@ -15,12 +15,23 @@ brew install ffmpeg
 brew tap homebrew/science
 brew install opencv
 
+# mayavi
+brew install --pyqt --python --qt vtk
+#brew install vtk --python
+#pip install traitsbackendqt
+pip install configobj
+pip install envisage
+pip install "Mayavi[app]"
+
 #machine learning
 pip install -U scikit-learn
 
 # neuralensemble
 pip install -U sumatra
 pip install hg+https://bitbucket.org/apdavison/sumatra
+
+# latex, bibliography and friends
+brew install bib-tool
 
 # Remove outdated versions from the cellar
 brew cleanup
