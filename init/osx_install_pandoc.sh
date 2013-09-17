3befe56b544209a4bf2f6cd36d8530af9f106ea9
Laurent Perrinet <laurent.perrinet@gmail.com>
2013-09-17 14:59:57 +0200

    installing pandoc on macosx

--- init/osx_install_pandoc.sh
+++ init/osx_install_pandoc.sh
@@ -6,5 +6,5 @@ echo "> installing "
 sudo installer -pkg /Volumes/pandoc\ 1.12/pandoc-1.12.pkg -target "/"
 echo "> cleaning up"
 umount /Volumes/pandoc\ 1.12
-rm -rf pandoc-1.12.dmgndoc-1.12.dmg
+rm -f pandoc-1.12.dmg
 echo "> installation finished "
