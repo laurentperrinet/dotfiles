dfd9dec66db922007ef1d4a5f7a09987f3f2e250
Laurent Perrinet <laurent.perrinet@gmail.com>
2013-09-18 15:13:53 +0200

    upgrading VirtualBox

--- init/osx_install_virtualbox.sh
+++ init/osx_install_virtualbox.sh
@@ -1,5 +1,5 @@
 cd /tmp
-wget http://download.virtualbox.org/virtualbox/4.2.16/VirtualBox-4.2.16-86992-OSX.dmg
-hdiutil attach VirtualBox-4.2.16-86992-OSX.dmg
+wget http://download.virtualbox.org/virtualbox/4.2.18/VirtualBox-4.2.18-88780-OSX.dmg
+hdiutil attach VirtualBox-4.2.18-88780-OSX.dmg
 sudo installer -pkg /Volumes/VirtualBox/VirtualBox.pkg -target /
 hdiutil detach /Volumes/VirtualBox
