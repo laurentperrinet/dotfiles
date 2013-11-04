9b65da34debfa8153adf30528913c0c3cf499789
Laurent Perrinet <laurent.perrinet@gmail.com>
2013-11-04 20:57:17 +0100

    git config uses osx credentials / modified merge style

--- init/osx_brew_bootstrap.sh
+++ init/osx_brew_bootstrap.sh
@@ -36,7 +36,6 @@ brew install bash-completion
 
 # Install everything else
 brew install git
-git config --global credential.helper osxkeychain
 #brew install rename
 #brew install rhino
 #brew install tree
