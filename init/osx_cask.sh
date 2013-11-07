386aaa6a91f923ba7340e8834d8ab4a8dba2dfa5
Laurent Perrinet <laurent.perrinet@gmail.com>
2013-11-07 10:30:41 +0100

    casking all my applications ...

--- init/osx_cask.sh
+++ init/osx_cask.sh
@@ -3,13 +3,14 @@ brew tap phinze/homebrew-cask
 brew install brew-cask
 
 function installcask() {
-	brew cask install -f "${@}" # 2> /dev/null
+	brew cask install "${@}" # 2> /dev/null
+	#brew cask install --force "${@}" # 2> /dev/null
 }
 
 installcask clamxav 
 installcask dropbox
 installcask google-chrome
-installcask google-chrome-canary
+#installcask google-chrome-canary
 #installcask imagealpha
 #installcask imageoptim
 installcask iterm2
