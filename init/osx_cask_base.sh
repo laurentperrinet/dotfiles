a329d5be850cf1ab387520717345f1bd7e2048b1
Laurent Perrinet <laurent.perrinet@gmail.com>
2013-11-10 12:06:27 +0100

    making 2 cask layers

--- init/osx_cask_base.sh
+++ init/osx_cask_base.sh
@@ -6,28 +6,18 @@ function installcask() {
 	brew cask install "${@}" # 2> /dev/null
 	#brew cask install --force "${@}" # 2> /dev/null
 }
-installcask bibdesk
 installcask clamxav 
 installcask dropbox
+installcask firefox
 installcask google-chrome
-#installcask google-chrome-canary
-#installcask imagealpha
-#installcask imageoptim
 installcask iterm2
 installcask macvim
-installcask mactex
-#installcask miro-video-converter
 installcask open-office
 installcask skype
 installcask sparkleshare
-installcask sourcetree
-installcask spyder
-#installcask sublime-text
 installcask the-unarchiver
 #installcask tor-browser
-#installcask transmission
 installcask tunnelblick unison
 installcask ukelele
-installcask virtualbox
 installcask vlc
 installcask x-quartz
--- init/osx_cask_full.sh
+++ init/osx_cask_full.sh
@@ -1,35 +1,10 @@
-# Install native apps
-brew tap phinze/homebrew-cask
-brew install brew-cask
+sh osx_cask_base.sh
 
-function installcask() {
-	brew cask install "${@}" # 2> /dev/null
-	#brew cask install --force "${@}" # 2> /dev/null
-}
+# Install native apps
 installcask bibdesk
-installcask bit-torrent-sync
-installcask clamxav 
-installcask dropbox
-installcask firefox
-installcask google-chrome
-#installcask google-chrome-canary
-#installcask imagealpha
-#installcask imageoptim
-installcask iterm2
-installcask macvim
+installcask bittorrent-sync
 installcask mactex
-#installcask miro-video-converter
-installcask open-office
-installcask skype
-installcask sparkleshare
-installcask sourcetree
 installcask spyder
-#installcask sublime-text
-installcask the-unarchiver
-#installcask tor-browser
-#installcask transmission
-installcask tunnelblick unison
-installcask ukelele
+installcask texshop
 installcask virtualbox
-installcask vlc
 installcask x-quartz
