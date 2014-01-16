096c3c7ff26581ca02f9123d45b10db56a3d7d98
Laurent Perrinet <laurent.perrinet@gmail.com>
2014-01-16 11:52:32 +0100

    updating pandoc

--- init/osx_install_pandoc.sh
+++ init/osx_install_pandoc.sh
@@ -1,10 +1,12 @@
 echo "> downloading pandoc "
-curl -OL http://pandoc.googlecode.com/files/pandoc-1.12.dmg
-echo "> mounting the image file "
-hdiutil attach pandoc-1.12.dmg
+curl -OL https://pandoc.googlecode.com/files/pandoc-1.12.3.pkg.zip
+unzip pandoc-1.12.3.pkg.zip
+#http://pandoc.googlecode.com/files/pandoc-1.12.dmg
+#echo "> mounting the image file "
+#hdiutil attach pandoc-1.12.dmg
 echo "> installing "
-sudo installer -pkg /Volumes/pandoc\ 1.12/pandoc-1.12.pkg -target "/"
+sudo installer -pkg pandoc-1.12.3.pkg -target "/"
 echo "> cleaning up"
-umount /Volumes/pandoc\ 1.12
-rm -f pandoc-1.12.dmg
+#umount /Volumes/pandoc\ 1.12
+rm -f pandoc-1.12.*
 echo "> installation finished "
