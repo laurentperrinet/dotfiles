54821265a1808dc66bad3890d8375e16e28fec3a
Laurent Perrinet <laurent.perrinet@gmail.com>
2014-02-10 21:36:40 +0100

    adding scripts to start / stop web radio

--- bin/start-fip.sh
+++ bin/start-fip.sh
@@ -1,1 +1,2 @@
-ssh love@mingus.local "open -a /Applications/Safari.app/ ~/Desktop/\>\>\ F\ -\ I\ -P\ \<\<.webloc"
+# ssh love@mingus.local "open -a /Applications/Safari.app/ ~/Desktop/\>\>\ F\ -\ I\ -P\ \<\<.webloc"
+ssh love@mingus.local "open -a /Applications/Safari.app/  http://www.fipradio.fr/player"
