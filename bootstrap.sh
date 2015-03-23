da34e296dabe47f27c09c8d26f52dcd7733c88bc
Laurent Perrinet <laurent.perrinet@gmail.com>
2015-03-23 10:02:07 +0100

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-          --exclude "libs/" --exclude "init/" \
+          --exclude ".osx" --exclude "libs/" --exclude "init/" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
--- init/osx_brew_casks.sh
+++ init/osx_brew_casks.sh
@@ -7,15 +7,13 @@ brew cask install clamxav
 brew cask uninstall bittorrent-sync
 brew cask install owncloud
 brew cask install dropbox
-brew cask uninstall wireshark
+brew cask install wireshark
 brew cask install java
 brew cask install flash
 brew cask install firefox
 brew cask install thunderbird
 brew cask install google-chrome
 brew cask install iterm2
-brew cask uninstall macvim
-#ln -s "/opt/homebrew-cask/Caskroom/macvim/7.4-72/MacVim-snapshot-72/mvim" "/usr/local/bin/mvim"
 brew tap caskroom/fonts
 brew cask install caskroom/fonts/font-symbola
 brew cask install caskroom/fonts/font-inconsolata.
@@ -29,14 +27,13 @@ brew cask uninstall ukelele
 brew cask install unison
 brew cask uninstall vimediamanager
 brew cask install vlc
-brew cask uninstall xbmc
+brew cask install spectacle
 brew cask install xquartz
 
 
 # quick look plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webp-quicklook suspicious-package && qlmanage -r
 defaults write com.apple.finder QLEnableTextSelection -bool true && killall Finder
-
 
 brew tap caskroom/versions
 brew cask install todotxt
