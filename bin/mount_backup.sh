2c4acc1e6111e62a654838535004fc831b823654
Laurent Perrinet <laurent.perrinet@gmail.com>
2015-04-28 09:42:14 +0200

    trying to make sshfs more UTF8 on mac using https://code.google.com/p/macfuse/issues/detail?id=139

--- bin/mount_backup.sh
+++ bin/mount_backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p ~/backup
-sshfs -p2225 admin@truc.hd.free.fr:/share/backup/ ~/backup
+sshfs -p2225 admin@truc.hd.free.fr:/share/backup/ ~/backup -orw,nodev,allow_other,reconnect,max_read=65536,compression=yes,auto_cache,no_check_root,kernel_cache,umask=0002,workaround=rename,auto_cache,defer_permissions,noappledouble,negative_vncache,intr,modules=iconv,from_code=UTF-8,to_code=UTF-8-MAC,volname=backup
